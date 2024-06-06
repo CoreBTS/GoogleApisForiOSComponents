36366449f1cce0300b34850e3116bb444797ea7d
bengavin <Ben.Gavin@corebts.com>
2024-06-05 21:10:49 -0500

    Further set-aside WIP

--- source/MLKit/FaceDetection/ApiDefinition.cs
+++ source/MLKit/FaceDetection/ApiDefinition.cs
@@ -6,280 +6,289 @@ using CoreGraphics;
 using ObjCRuntime;
 
 using MLKit.Core;
+using MLKit.Vision;
 
 #if !NET
 using NativeHandle = System.IntPtr;
 #endif
 
-namespace MLKit.FaceDetection {
+namespace MLKit.FaceDetection
+{
 
 	[Static]
-	partial interface FaceContours {
+	partial interface FaceContours
+	{
 		// extern const MLKFaceContourType _Nonnull MLKFaceContourTypeFace;
-		[Field ("MLKFaceContourTypeFace", "__Internal")]
+		[Field("MLKFaceContourTypeFace", "__Internal")]
 		NSString Face { get; }
 
 		// extern const MLKFaceContourType _Nonnull MLKFaceContourTypeLeftEyebrowTop;
-		[Field ("MLKFaceContourTypeLeftEyebrowTop", "__Internal")]
+		[Field("MLKFaceContourTypeLeftEyebrowTop", "__Internal")]
 		NSString LeftEyebrowTop { get; }
 
 		// extern const MLKFaceContourType _Nonnull MLKFaceContourTypeLeftEyebrowBottom;
-		[Field ("MLKFaceContourTypeLeftEyebrowBottom", "__Internal")]
+		[Field("MLKFaceContourTypeLeftEyebrowBottom", "__Internal")]
 		NSString eftEyebrowBottom { get; }
 
 		// extern const MLKFaceContourType _Nonnull MLKFaceContourTypeRightEyebrowTop;
-		[Field ("MLKFaceContourTypeRightEyebrowTop", "__Internal")]
+		[Field("MLKFaceContourTypeRightEyebrowTop", "__Internal")]
 		NSString RightEyebrowTop { get; }
 
 		// extern const MLKFaceContourType _Nonnull MLKFaceContourTypeRightEyebrowBottom;
-		[Field ("MLKFaceContourTypeRightEyebrowBottom", "__Internal")]
+		[Field("MLKFaceContourTypeRightEyebrowBottom", "__Internal")]
 		NSString RightEyebrowBottom { get; }
 
 		// extern const MLKFaceContourType _Nonnull MLKFaceContourTypeLeftEye;
-		[Field ("MLKFaceContourTypeLeftEye", "__Internal")]
+		[Field("MLKFaceContourTypeLeftEye", "__Internal")]
 		NSString LeftEye { get; }
 
 		// extern const MLKFaceContourType _Nonnull MLKFaceContourTypeRightEye;
-		[Field ("MLKFaceContourTypeRightEye", "__Internal")]
+		[Field("MLKFaceContourTypeRightEye", "__Internal")]
 		NSString RightEye { get; }
 
 		// extern const MLKFaceContourType _Nonnull MLKFaceContourTypeUpperLipTop;
-		[Field ("MLKFaceContourTypeUpperLipTop", "__Internal")]
+		[Field("MLKFaceContourTypeUpperLipTop", "__Internal")]
 		NSString UpperLipTop { get; }
 
 		// extern const MLKFaceContourType _Nonnull MLKFaceContourTypeUpperLipBottom;
-		[Field ("MLKFaceContourTypeUpperLipBottom", "__Internal")]
+		[Field("MLKFaceContourTypeUpperLipBottom", "__Internal")]
 		NSString UpperLipBottom { get; }
 
 		// extern const MLKFaceContourType _Nonnull MLKFaceContourTypeLowerLipTop;
-		[Field ("MLKFaceContourTypeLowerLipTop", "__Internal")]
+		[Field("MLKFaceContourTypeLowerLipTop", "__Internal")]
 		NSString LowerLipTop { get; }
 
 		// extern const MLKFaceContourType _Nonnull MLKFaceContourTypeLowerLipBottom;
-		[Field ("MLKFaceContourTypeLowerLipBottom", "__Internal")]
+		[Field("MLKFaceContourTypeLowerLipBottom", "__Internal")]
 		NSString LowerLipBottom { get; }
 
 		// extern const MLKFaceContourType _Nonnull MLKFaceContourTypeNoseBridge;
-		[Field ("MLKFaceContourTypeNoseBridge", "__Internal")]
+		[Field("MLKFaceContourTypeNoseBridge", "__Internal")]
 		NSString NoseBridge { get; }
 
 		// extern const MLKFaceContourType _Nonnull MLKFaceContourTypeNoseBottom;
-		[Field ("MLKFaceContourTypeNoseBottom", "__Internal")]
+		[Field("MLKFaceContourTypeNoseBottom", "__Internal")]
 		NSString NoseBottom { get; }
 
 		// extern const MLKFaceContourType _Nonnull MLKFaceContourTypeLeftCheek;
-		[Field ("MLKFaceContourTypeLeftCheek", "__Internal")]
+		[Field("MLKFaceContourTypeLeftCheek", "__Internal")]
 		NSString LeftCheek { get; }
 
 		// extern const MLKFaceContourType _Nonnull MLKFaceContourTypeRightCheek;
-		[Field ("MLKFaceContourTypeRightCheek", "__Internal")]
+		[Field("MLKFaceContourTypeRightCheek", "__Internal")]
 		NSString RightCheek { get; }
 	}
 
 	// @interface MLKFaceContour : NSObject
-	[BaseType (typeof (NSObject), Name = "MLKFaceContour")]
+	[BaseType(typeof(NSObject), Name = "MLKFaceContour")]
 	[DisableDefaultCtor]
-	interface FaceContour {
+	interface FaceContour
+	{
 		// @property (readonly, nonatomic) MLKFaceContourType _Nonnull type;
-		[Export ("type")]
+		[Export("type")]
 		string Type { get; }
 
 		// @property (readonly, nonatomic) NSArray<MLKVisionPoint *> * _Nonnull points;
-		[Export ("points")]
-		VisionPoint [] Points { get; }
+		[Export("points")]
+		VisionPoint[] Points { get; }
 	}
 
 	[Static]
-	partial interface FaceLandmarks {
+	partial interface FaceLandmarks
+	{
 		// extern const MLKFaceLandmarkType _Nonnull MLKFaceLandmarkTypeMouthBottom;
-		[Field ("MLKFaceLandmarkTypeMouthBottom", "__Internal")]
+		[Field("MLKFaceLandmarkTypeMouthBottom", "__Internal")]
 		NSString MouthBottom { get; }
 
 		// extern const MLKFaceLandmarkType _Nonnull MLKFaceLandmarkTypeMouthRight;
-		[Field ("MLKFaceLandmarkTypeMouthRight", "__Internal")]
+		[Field("MLKFaceLandmarkTypeMouthRight", "__Internal")]
 		NSString MouthRight { get; }
 
 		// extern const MLKFaceLandmarkType _Nonnull MLKFaceLandmarkTypeMouthLeft;
-		[Field ("MLKFaceLandmarkTypeMouthLeft", "__Internal")]
+		[Field("MLKFaceLandmarkTypeMouthLeft", "__Internal")]
 		NSString MouthLeft { get; }
 
 		// extern const MLKFaceLandmarkType _Nonnull MLKFaceLandmarkTypeLeftEar;
-		[Field ("MLKFaceLandmarkTypeLeftEar", "__Internal")]
+		[Field("MLKFaceLandmarkTypeLeftEar", "__Internal")]
 		NSString LeftEar { get; }
 
 		// extern const MLKFaceLandmarkType _Nonnull MLKFaceLandmarkTypeRightEar;
-		[Field ("MLKFaceLandmarkTypeRightEar", "__Internal")]
+		[Field("MLKFaceLandmarkTypeRightEar", "__Internal")]
 		NSString RightEar { get; }
 
 		// extern const MLKFaceLandmarkType _Nonnull MLKFaceLandmarkTypeLeftEye;
-		[Field ("MLKFaceLandmarkTypeLeftEye", "__Internal")]
+		[Field("MLKFaceLandmarkTypeLeftEye", "__Internal")]
 		NSString LeftEye { get; }
 
 		// extern const MLKFaceLandmarkType _Nonnull MLKFaceLandmarkTypeRightEye;
-		[Field ("MLKFaceLandmarkTypeRightEye", "__Internal")]
+		[Field("MLKFaceLandmarkTypeRightEye", "__Internal")]
 		NSString RightEye { get; }
 
 		// extern const MLKFaceLandmarkType _Nonnull MLKFaceLandmarkTypeLeftCheek;
-		[Field ("MLKFaceLandmarkTypeLeftCheek", "__Internal")]
+		[Field("MLKFaceLandmarkTypeLeftCheek", "__Internal")]
 		NSString LeftCheek { get; }
 
 		// extern const MLKFaceLandmarkType _Nonnull MLKFaceLandmarkTypeRightCheek;
-		[Field ("MLKFaceLandmarkTypeRightCheek", "__Internal")]
+		[Field("MLKFaceLandmarkTypeRightCheek", "__Internal")]
 		NSString RightCheek { get; }
 
 		// extern const MLKFaceLandmarkType _Nonnull MLKFaceLandmarkTypeNoseBase;
-		[Field ("MLKFaceLandmarkTypeNoseBase", "__Internal")]
+		[Field("MLKFaceLandmarkTypeNoseBase", "__Internal")]
 		NSString NoseBase { get; }
 	}
 
 	// @interface MLKFaceLandmark : NSObject
-	[BaseType (typeof (NSObject), Name = "MLKFaceLandmark")]
+	[BaseType(typeof(NSObject), Name = "MLKFaceLandmark")]
 	[DisableDefaultCtor]
-	interface FaceLandmark {
+	interface FaceLandmark
+	{
 		// @property (readonly, nonatomic) MLKFaceLandmarkType _Nonnull type;
-		[Export ("type")]
+		[Export("type")]
 		string Type { get; }
 
 		// @property (readonly, nonatomic) MLKVisionPoint * _Nonnull position;
-		[Export ("position")]
+		[Export("position")]
 		VisionPoint Position { get; }
 	}
 
 	// @interface MLKFace : NSObject
-	[BaseType (typeof (NSObject), Name = "MLKFace")]
+	[BaseType(typeof(NSObject), Name = "MLKFace")]
 	[DisableDefaultCtor]
-	interface Face {
+	interface Face
+	{
 		// @property (readonly, nonatomic) CGRect frame;
-		[Export ("frame")]
+		[Export("frame")]
 		CGRect Frame { get; }
 
 		// @property (readonly, nonatomic) NSArray<MLKFaceLandmark *> * _Nonnull landmarks;
-		[Export ("landmarks")]
-		FaceLandmark [] Landmarks { get; }
+		[Export("landmarks")]
+		FaceLandmark[] Landmarks { get; }
 
 		// @property (readonly, nonatomic) NSArray<MLKFaceContour *> * _Nonnull contours;
-		[Export ("contours")]
-		FaceContour [] Contours { get; }
+		[Export("contours")]
+		FaceContour[] Contours { get; }
 
 		// @property (readonly, nonatomic) BOOL hasTrackingID;
-		[Export ("hasTrackingID")]
+		[Export("hasTrackingID")]
 		bool HasTrackingID { get; }
 
 		// @property (readonly, nonatomic) NSInteger trackingID;
-		[Export ("trackingID")]
+		[Export("trackingID")]
 		nint TrackingID { get; }
 
 		// @property (readonly, nonatomic) BOOL hasHeadEulerAngleX;
-		[Export ("hasHeadEulerAngleX")]
+		[Export("hasHeadEulerAngleX")]
 		bool HasHeadEulerAngleX { get; }
 
 		// @property (readonly, nonatomic) CGFloat headEulerAngleX;
-		[Export ("headEulerAngleX")]
+		[Export("headEulerAngleX")]
 		nfloat HeadEulerAngleX { get; }
 
 		// @property (readonly, nonatomic) BOOL hasHeadEulerAngleY;
-		[Export ("hasHeadEulerAngleY")]
+		[Export("hasHeadEulerAngleY")]
 		bool HasHeadEulerAngleY { get; }
 
 		// @property (readonly, nonatomic) CGFloat headEulerAngleY;
-		[Export ("headEulerAngleY")]
+		[Export("headEulerAngleY")]
 		nfloat HeadEulerAngleY { get; }
 
 		// @property (readonly, nonatomic) BOOL hasHeadEulerAngleZ;
-		[Export ("hasHeadEulerAngleZ")]
+		[Export("hasHeadEulerAngleZ")]
 		bool HasHeadEulerAngleZ { get; }
 
 		// @property (readonly, nonatomic) CGFloat headEulerAngleZ;
-		[Export ("headEulerAngleZ")]
+		[Export("headEulerAngleZ")]
 		nfloat HeadEulerAngleZ { get; }
 
 		// @property (readonly, nonatomic) BOOL hasSmilingProbability;
-		[Export ("hasSmilingProbability")]
+		[Export("hasSmilingProbability")]
 		bool HasSmilingProbability { get; }
 
 		// @property (readonly, nonatomic) CGFloat smilingProbability;
-		[Export ("smilingProbability")]
+		[Export("smilingProbability")]
 		nfloat SmilingProbability { get; }
 
 		// @property (readonly, nonatomic) BOOL hasLeftEyeOpenProbability;
-		[Export ("hasLeftEyeOpenProbability")]
+		[Export("hasLeftEyeOpenProbability")]
 		bool HasLeftEyeOpenProbability { get; }
 
 		// @property (readonly, nonatomic) CGFloat leftEyeOpenProbability;
-		[Export ("leftEyeOpenProbability")]
+		[Export("leftEyeOpenProbability")]
 		nfloat LeftEyeOpenProbability { get; }
 
 		// @property (readonly, nonatomic) BOOL hasRightEyeOpenProbability;
-		[Export ("hasRightEyeOpenProbability")]
+		[Export("hasRightEyeOpenProbability")]
 		bool HasRightEyeOpenProbability { get; }
 
 		// @property (readonly, nonatomic) CGFloat rightEyeOpenProbability;
-		[Export ("rightEyeOpenProbability")]
+		[Export("rightEyeOpenProbability")]
 		nfloat RightEyeOpenProbability { get; }
 
 		// -(MLKFaceLandmark * _Nullable)landmarkOfType:(MLKFaceLandmarkType _Nonnull)type;
-		[Export ("landmarkOfType:")]
+		[Export("landmarkOfType:")]
 		[return: NullAllowed]
-		FaceLandmark LandmarkOfType (string type);
+		FaceLandmark LandmarkOfType(string type);
 
 		// -(MLKFaceContour * _Nullable)contourOfType:(MLKFaceContourType _Nonnull)type;
-		[Export ("contourOfType:")]
+		[Export("contourOfType:")]
 		[return: NullAllowed]
-		FaceContour ContourOfType (string type);
+		FaceContour ContourOfType(string type);
 	}
 
 	// typedef void (^MLKFaceDetectionCallback)(NSArray<MLKFace *> * _Nullable, NSError * _Nullable);
-	delegate void FaceDetectionCallback ([NullAllowed] Face [] faces, [NullAllowed] NSError error);
+	delegate void FaceDetectionCallback([NullAllowed] Face[] faces, [NullAllowed] NSError error);
 
 	// @interface MLKFaceDetector : NSObject
-	[BaseType (typeof (NSObject), Name = "MLKFaceDetector")]
+	[BaseType(typeof(NSObject), Name = "MLKFaceDetector")]
 	[DisableDefaultCtor]
-	interface FaceDetector {
+	interface FaceDetector
+	{
 		// +(instancetype _Nonnull)faceDetectorWithOptions:(MLKFaceDetectorOptions * _Nonnull)options __attribute__((swift_name("faceDetector(options:)")));
 		[Static]
-		[Export ("faceDetectorWithOptions:")]
-		FaceDetector FaceDetectorWithOptions (FaceDetectorOptions options);
+		[Export("faceDetectorWithOptions:")]
+		FaceDetector FaceDetectorWithOptions(FaceDetectorOptions options);
 
 		// +(instancetype _Nonnull)faceDetector __attribute__((swift_name("faceDetector()")));
 		[Static]
-		[Export ("faceDetector")]
+		[Export("faceDetector")]
 		FaceDetector DefaultInstance { get; }
 
 		// -(void)processImage:(id<MLKCompatibleImage> _Nonnull)image completion:(MLKFaceDetectionCallback _Nonnull)completion __attribute__((swift_name("process(_:completion:)")));
-		[Export ("processImage:completion:")]
-		void ProcessImage (ICompatibleImage image, FaceDetectionCallback completion);
+		[Export("processImage:completion:")]
+		void ProcessImage(CompatibleImage image, FaceDetectionCallback completion);
 
 		// -(NSArray<MLKFace *> * _Nullable)resultsInImage:(id<MLKCompatibleImage> _Nonnull)image error:(NSError * _Nullable * _Nullable)error;
-		[Export ("resultsInImage:error:")]
+		[Export("resultsInImage:error:")]
 		[return: NullAllowed]
-		Face [] ResultsInImage (ICompatibleImage image, [NullAllowed] out NSError error);
+		Face[] ResultsInImage(CompatibleImage image, [NullAllowed] out NSError error);
 	}
 
 	// @interface MLKFaceDetectorOptions : NSObject
-	[BaseType (typeof (NSObject), Name = "MLKFaceDetectorOptions")]
-	interface FaceDetectorOptions {
+	[BaseType(typeof(NSObject), Name = "MLKFaceDetectorOptions")]
+	interface FaceDetectorOptions
+	{
 		// @property (nonatomic) MLKFaceDetectorClassificationMode classificationMode;
-		[Export ("classificationMode")]
+		[Export("classificationMode")]
 		FaceClassificationMode ClassificationMode { get; set; }
 
 		// @property (nonatomic) MLKFaceDetectorPerformanceMode performanceMode;
-		[Export ("performanceMode")]
+		[Export("performanceMode")]
 		FacePerformanceMode PerformanceMode { get; set; }
 
 		// @property (nonatomic) MLKFaceDetectorLandmarkMode landmarkMode;
-		[Export ("landmarkMode")]
+		[Export("landmarkMode")]
 		FaceLandmarkMode LandmarkMode { get; set; }
 
 		// @property (nonatomic) MLKFaceDetectorContourMode contourMode;
-		[Export ("contourMode")]
+		[Export("contourMode")]
 		FaceContourMode ContourMode { get; set; }
 
 		// @property (nonatomic) CGFloat minFaceSize;
-		[Export ("minFaceSize")]
+		[Export("minFaceSize")]
 		nfloat MinFaceSize { get; set; }
 
 		// @property (getter = isTrackingEnabled, nonatomic) BOOL trackingEnabled;
-		[Export ("trackingEnabled")]
-		bool TrackingEnabled { [Bind ("isTrackingEnabled")] get; set; }
+		[Export("trackingEnabled")]
+		bool TrackingEnabled { [Bind("isTrackingEnabled")] get; set; }
 	}
 }
--- source/MLKit/TextRecognition/ApiDefinition.cs
+++ source/MLKit/TextRecognition/ApiDefinition.cs
@@ -6,129 +6,138 @@ using CoreGraphics;
 using ObjCRuntime;
 
 using MLKit.Core;
+using MLKit.Vision;
 
 #if !NET
 using NativeHandle = System.IntPtr;
 #endif
 
-namespace MLKit.TextRecognition {
+namespace MLKit.TextRecognition
+{
 	// @interface MLKCommonTextRecognizerOptions : NSObject
-	[BaseType (typeof (NSObject), Name = "MLKCommonTextRecognizerOptions")]
+	[BaseType(typeof(NSObject), Name = "MLKCommonTextRecognizerOptions")]
 	[DisableDefaultCtor]
-	interface CommonTextRecognizerOptions {
+	interface CommonTextRecognizerOptions
+	{
 	}
 
 	// @interface MLKText : NSObject
-	[BaseType (typeof (NSObject), Name = "MLKText")]
+	[BaseType(typeof(NSObject), Name = "MLKText")]
 	[DisableDefaultCtor]
-	interface VisionText {
+	interface VisionText
+	{
 		// @property (readonly, nonatomic) NSString * _Nonnull text;
-		[Export ("text")]
+		[Export("text")]
 		string Text { get; }
 
 		// @property (readonly, nonatomic) NSArray<MLKTextBlock *> * _Nonnull blocks;
-		[Export ("blocks")]
-		VisionTextBlock [] Blocks { get; }
+		[Export("blocks")]
+		VisionTextBlock[] Blocks { get; }
 	}
 
 	// @interface MLKTextBlock : NSObject
-	[BaseType (typeof (NSObject), Name = "MLKTextBlock")]
+	[BaseType(typeof(NSObject), Name = "MLKTextBlock")]
 	[DisableDefaultCtor]
-	interface VisionTextBlock {
+	interface VisionTextBlock
+	{
 		// @property (readonly, nonatomic) NSString * _Nonnull text;
-		[Export ("text")]
+		[Export("text")]
 		string Text { get; }
 
 		// @property (readonly, nonatomic) NSArray<MLKTextLine *> * _Nonnull lines;
-		[Export ("lines")]
-		VisionTextLine [] Lines { get; }
+		[Export("lines")]
+		VisionTextLine[] Lines { get; }
 
 		// @property (readonly, nonatomic) CGRect frame;
-		[Export ("frame")]
+		[Export("frame")]
 		CGRect Frame { get; }
 
 		// @property (readonly, nonatomic) NSArray<MLKTextRecognizedLanguage *> * _Nonnull recognizedLanguages;
-		[Export ("recognizedLanguages")]
-		TextRecognizedLanguage [] RecognizedLanguages { get; }
+		[Export("recognizedLanguages")]
+		TextRecognizedLanguage[] RecognizedLanguages { get; }
 
 		// @property (readonly, nonatomic) NSArray<NSValue *> * _Nonnull cornerPoints;
-		[Export ("cornerPoints")]
-		NSValue [] CornerPoints { get; }
+		[Export("cornerPoints")]
+		NSValue[] CornerPoints { get; }
 	}
 
 	// @interface MLKTextElement : NSObject
-	[BaseType (typeof (NSObject), Name = "MLKTextElement")]
+	[BaseType(typeof(NSObject), Name = "MLKTextElement")]
 	[DisableDefaultCtor]
-	interface VisionTextElement {
+	interface VisionTextElement
+	{
 		// @property (readonly, nonatomic) NSString * _Nonnull text;
-		[Export ("text")]
+		[Export("text")]
 		string Text { get; }
 
 		// @property (readonly, nonatomic) CGRect frame;
-		[Export ("frame")]
+		[Export("frame")]
 		CGRect Frame { get; }
 
 		// @property (readonly, nonatomic) NSArray<MLKTextRecognizedLanguage *> * _Nonnull recognizedLanguages;
-		[Export ("recognizedLanguages")]
-		TextRecognizedLanguage [] RecognizedLanguages { get; }
+		[Export("recognizedLanguages")]
+		TextRecognizedLanguage[] RecognizedLanguages { get; }
 
 		// @property (readonly, nonatomic) NSArray<NSValue *> * _Nonnull cornerPoints;
-		[Export ("cornerPoints")]
-		NSValue [] CornerPoints { get; }
+		[Export("cornerPoints")]
+		NSValue[] CornerPoints { get; }
 	}
 
 	// @interface MLKTextLine : NSObject
-	[BaseType (typeof (NSObject), Name = "MLKTextLine")]
+	[BaseType(typeof(NSObject), Name = "MLKTextLine")]
 	[DisableDefaultCtor]
-	interface VisionTextLine {
+	interface VisionTextLine
+	{
 		// @property (readonly, nonatomic) NSString * _Nonnull text;
-		[Export ("text")]
+		[Export("text")]
 		string Text { get; }
 
 		// @property (readonly, nonatomic) NSArray<MLKTextElement *> * _Nonnull elements;
-		[Export ("elements")]
-		VisionTextElement [] Elements { get; }
+		[Export("elements")]
+		VisionTextElement[] Elements { get; }
 
 		// @property (readonly, nonatomic) CGRect frame;
-		[Export ("frame")]
+		[Export("frame")]
 		CGRect Frame { get; }
 
 		// @property (readonly, nonatomic) NSArray<MLKTextRecognizedLanguage *> * _Nonnull recognizedLanguages;
-		[Export ("recognizedLanguages")]
-		TextRecognizedLanguage [] RecognizedLanguages { get; }
+		[Export("recognizedLanguages")]
+		TextRecognizedLanguage[] RecognizedLanguages { get; }
 
 		// @property (readonly, nonatomic) NSArray<NSValue *> * _Nonnull cornerPoints;
-		[Export ("cornerPoints")]
-		NSValue [] CornerPoints { get; }
+		[Export("cornerPoints")]
+		NSValue[] CornerPoints { get; }
 	}
 
 	// @interface MLKTextRecognizedLanguage : NSObject
-	[BaseType (typeof (NSObject), Name = "MLKTextRecognizedLanguage")]
+	[BaseType(typeof(NSObject), Name = "MLKTextRecognizedLanguage")]
 	[DisableDefaultCtor]
-	interface TextRecognizedLanguage {
+	interface TextRecognizedLanguage
+	{
 		// @property (readonly, nonatomic) NSString * _Nullable languageCode;
-		[NullAllowed, Export ("languageCode")]
+		[NullAllowed, Export("languageCode")]
 		string LanguageCode { get; }
 	}
 
 	// typedef void (^MLKTextRecognitionCallback)(MLKText * _Nullable, NSError * _Nullable);
-	delegate void TextRecognitionCallback ([NullAllowed] VisionText text, [NullAllowed] NSError error);
+	delegate void TextRecognitionCallback([NullAllowed] VisionText text, [NullAllowed] NSError error);
 
 	// @interface MLKTextRecognizer : NSObject
-	[BaseType (typeof (NSObject), Name = "MLKTextRecognizer")]
-	interface TextRecognizer {
+	[BaseType(typeof(NSObject), Name = "MLKTextRecognizer")]
+	interface TextRecognizer
+	{
 		// +(instancetype _Nonnull)textRecognizerWithOptions:(MLKCommonTextRecognizerOptions * _Nonnull)options __attribute__((swift_name("textRecognizer(options:)")));
 		[Static]
-		[Export ("textRecognizerWithOptions:")]
-		TextRecognizer TextRecognizerWithOptions (CommonTextRecognizerOptions options);
+		[Export("textRecognizerWithOptions:")]
+		TextRecognizer TextRecognizerWithOptions(CommonTextRecognizerOptions options);
 
 		// -(void)processImage:(id<MLKCompatibleImage> _Nonnull)image completion:(MLKTextRecognitionCallback _Nonnull)completion __attribute__((swift_name("process(_:completion:)")));
-		[Export ("processImage:completion:")]
-		void ProcessImage (ICompatibleImage image, TextRecognitionCallback completion);
+		[Export("processImage:completion:")]
+		void ProcessImage(CompatibleImage image, TextRecognitionCallback completion);
 
 		// -(MLKText * _Nullable)resultsInImage:(id<MLKCompatibleImage> _Nonnull)image error:(NSError * _Nullable * _Nullable)error;
-		[Export ("resultsInImage:error:")]
+		[Export("resultsInImage:error:")]
 		[return: NullAllowed]
-		VisionText ResultsInImage (ICompatibleImage image, [NullAllowed] out NSError error);
+		VisionText ResultsInImage(CompatibleImage image, [NullAllowed] out NSError error);
 	}
 }
